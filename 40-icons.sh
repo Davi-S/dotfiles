bb5bf511466473f5bc03295ece02c9544deefde8
Davi-S <67160664+Davi-S@users.noreply.github.com>
2025-02-03 19:58:13 -0300

    add cursor icon

---
+++ 40-icons.sh
@@ -0,0 +1,34 @@
+# This file constain the icons in "~/.local/share/"
+
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/background.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/beam.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/default.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/forbidden.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/hand.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/help.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/move.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/pen.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/precision.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/right_ptr.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/size_EW.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/size_NS.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/size_NeSw.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/size_NwSe.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors/watch.hlc '' davi davi
+CopyFile /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/manifest.hl '' davi davi
+
+
+# File properties
+
+SetFileProperty /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors group davi
+SetFileProperty /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono/hyprcursors owner davi
+SetFileProperty /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono group davi
+SetFileProperty /home/davi/.local/share/icons/theme_Posys-Cursor-Scalable-Mono owner davi
+SetFileProperty /home/davi/.local/share/icons group davi
+SetFileProperty /home/davi/.local/share/icons owner davi
+SetFileProperty /home/davi/.local/share/sddm group davi
+SetFileProperty /home/davi/.local/share/sddm owner davi
+SetFileProperty /home/davi/.local/share group davi
+SetFileProperty /home/davi/.local/share owner davi
+SetFileProperty /home/davi/.local group davi
+SetFileProperty /home/davi/.local owner davi
